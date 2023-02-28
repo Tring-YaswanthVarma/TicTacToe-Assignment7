ce06253029e5fedeaf0dc6c1d446cd0972ce3fbe
Tringapps-YaswanthVarma <konduru.varma@tringapps.com>
2023-02-28 17:29:27 +0530

    first commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -11,10 +11,8 @@ class Main {
     public static void print(char[][] a) {
         System.out.println("  0 1 2\n");
         for (int i = 0; i < 3; i++) {
-//            s = i + " ";
             System.out.print(i + " ");
             for (int j = 0; j < 3; j++) {
-//                s = a[i][j] + " ";
                 System.out.print(a[i][j] + " ");
             }
             System.out.println("\n");
@@ -23,7 +21,6 @@ class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         Logger logger = Logger.getLogger("com.api.jar");
-//        System.out.println("Hello how are you :");
         char[][] a = new char[3][3];
         TicTacToe t = new TicTacToe();
         char p1 = 'x';
@@ -33,9 +30,6 @@ class Main {
         int columnIndex = -1;
         int count = 0;
         while (count < 9) {
-//            p = p2;
-//            if (count % 2 == 0)
-//                p = p1;
             p = count % 2 == 0 ? p1 : p2;
             String s;
             print(a);
@@ -51,9 +45,6 @@ class Main {
                         player = false;
                     else
                         System.out.println("That position is already occupied. Try again");
-//                } catch (InputMismatchException e) {
-//                    System.out.println("Cannot enter alphabets. Enter a number from 0, 1 and 2.");
-//                    sc.nextLine();
                 } catch (Exception e){
                     System.out.println("Only enter a number less than 3, Since this is a 3x3 grid.");
                 }
@@ -64,7 +55,6 @@ class Main {
                 System.out.println("Player " + p + " Win");
                 break;
             }
-//            System.out.println("continue---------");
             count += 1;
         }
     }
