8ae3124712ab2c2cfc83c1dc643af12d5e37df6c
Tringapps-YaswanthVarma <konduru.varma@tringapps.com>
2023-02-28 17:45:47 +0530

    first commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,20 +1,25 @@
 package org.example;
 
 import java.util.Scanner;
+import java.util.logging.Logger;
 
 class Main {
 
     public static void print(char[][] a) {
-        System.out.println("  0 1 2\n");
+        Logger log = Logger.getLogger("com.api.jar");
+
+        log.info("  0 1 2\n");
         for (int i = 0; i < 3; i++) {
-            System.out.print(i + " ");
+            log.info(i + " ");
             for (int j = 0; j < 3; j++) {
-                System.out.print(a[i][j] + " ");
+                log.info(a[i][j] + " ");
             }
-            System.out.println("\n");
+            log.info("\n");
         }
     }
     public static void main(String[] args) {
+        Logger log = Logger.getLogger("com.api.jar");
+
         Scanner sc = new Scanner(System.in);
         char[][] a = new char[3][3];
         TicTacToe t = new TicTacToe();
@@ -29,24 +34,24 @@ class Main {
             print(a);
             boolean player = true;
             while (player) {
-                System.out.println("Player " + p + " turn : ");
+                log.info("Player " + p + " turn : ");
                 try {
 
-                    System.out.println("Enter row Index and column index: ");
+                    log.info("Enter row Index and column index: ");
                     rowIndex = sc.nextInt();
                     columnIndex = sc.nextInt();
                     if (a[rowIndex][columnIndex] == 0)
                         player = false;
                     else
-                        System.out.println("That position is already occupied. Try again");
+                        log.info("That position is already occupied. Try again");
                 } catch (Exception e){
-                    System.out.println("Only enter a number less than 3, Since this is a 3x3 grid.");
+                    log.info("Only enter a number less than 3, Since this is a 3x3 grid.");
                 }
 
             }
             a[rowIndex][columnIndex] = p;
             if (t.checkWin(rowIndex, columnIndex, a) == 0) {
-                System.out.println("Player " + p + " Win");
+                log.info("Player " + p + " Win");
                 break;
             }
             count += 1;
